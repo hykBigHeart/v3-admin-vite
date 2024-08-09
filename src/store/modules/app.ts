388f8cc7573764913eca361290ada8f4891468e5
hykBigHeart <3069479901@qq.com>
2024-08-09 17:02:33 +0800

    1. pinia数据持久化

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -13,43 +13,52 @@ function handleSidebarStatus(opened: boolean) {
   opened ? setSidebarStatus(SIDEBAR_OPENED) : setSidebarStatus(SIDEBAR_CLOSED)
 }
 
-export const useAppStore = defineStore("app", () => {
-  /** 侧边栏状态 */
-  const sidebar: Sidebar = reactive({
-    opened: getSidebarStatus() !== SIDEBAR_CLOSED,
-    withoutAnimation: false
-  })
-  /** 设备类型 */
-  const device = ref<DeviceEnum>(DeviceEnum.Desktop)
+export const useAppStore = defineStore(
+  "app",
+  () => {
+    /** 侧边栏状态 */
+    const sidebar: Sidebar = reactive({
+      opened: getSidebarStatus() !== SIDEBAR_CLOSED,
+      withoutAnimation: false
+    })
+    /** 设备类型 */
+    const device = ref<DeviceEnum>(DeviceEnum.Desktop)
 
-  /** 版本名称 */
-  const version = ref("第一")
+    /** 版本名称 */
+    const version = ref("第一")
 
-  /** 监听侧边栏 opened 状态 */
-  watch(
-    () => sidebar.opened,
-    (opened) => handleSidebarStatus(opened)
-  )
+    /** 监听侧边栏 opened 状态 */
+    watch(
+      () => sidebar.opened,
+      (opened) => handleSidebarStatus(opened)
+    )
 
-  /** 切换侧边栏 */
-  const toggleSidebar = (withoutAnimation: boolean) => {
-    sidebar.opened = !sidebar.opened
-    sidebar.withoutAnimation = withoutAnimation
+    /** 切换侧边栏 */
+    const toggleSidebar = (withoutAnimation: boolean) => {
+      sidebar.opened = !sidebar.opened
+      sidebar.withoutAnimation = withoutAnimation
+    }
+    /** 关闭侧边栏 */
+    const closeSidebar = (withoutAnimation: boolean) => {
+      sidebar.opened = false
+      sidebar.withoutAnimation = withoutAnimation
+    }
+    /** 切换设备类型 */
+    const toggleDevice = (value: DeviceEnum) => {
+      device.value = value
+    }
+
+    /** 切换版本 */
+    const toggleVersion = (value: string) => {
+      version.value = value
+    }
+
+    return { device, sidebar, version, toggleSidebar, closeSidebar, toggleDevice, toggleVersion }
+  },
+  {
+    persist: {
+      key: "app-store",
+      storage: sessionStorage
+    }
   }
-  /** 关闭侧边栏 */
-  const closeSidebar = (withoutAnimation: boolean) => {
-    sidebar.opened = false
-    sidebar.withoutAnimation = withoutAnimation
-  }
-  /** 切换设备类型 */
-  const toggleDevice = (value: DeviceEnum) => {
-    device.value = value
-  }
-
-  /** 切换版本 */
-  const toggleVersion = (value: string) => {
-    version.value = value
-  }
-
-  return { device, sidebar, version, toggleSidebar, closeSidebar, toggleDevice, toggleVersion }
-})
+)
